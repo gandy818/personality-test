4343d0709c497dd6b8cddc954b10475d03a1f955
gandy818 <gandy818@gmail.com>
2024-08-08 09:22:05 +0900

    feat: daisyui 설치, 폰트 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main>메인페이지</main>
+    <main>메인페이지
+      <button className="btn">Button</button>
+    </main>
   );
 }
