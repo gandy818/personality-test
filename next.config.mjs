32875d9cbd123d91e594ee6fcc6151766d5d9e8b
gandy818 <gandy818@gmail.com>
2024-09-11 22:21:39 +0900

    fix: config 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', trailingSlash: true };
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
 export default nextConfig;
