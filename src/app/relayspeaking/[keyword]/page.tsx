4bc9b3285ee66ac95dfa760994c1507abd0c89fb
gandy818 <gandy818@gmail.com>
2024-10-06 17:48:12 +0900

    feat: 이어말하기 데이터 추가, 스피드게임 단어 소진시 타이머 종료 로직 추가

--- src/app/relayspeaking/[keyword]/page.tsx
+++ src/app/relayspeaking/[keyword]/page.tsx
@@ -1,8 +1,9 @@
+import RelayspeakingComponent from '@/components/relayspeaking/RelayspeakingComponent';
 import Link from 'next/link';
 
 // 빌드를 위한 경로 미리 설정
 export async function generateStaticParams() {
-  const keywords = ['fruit'];
+  const keywords = ['proverb', 'idiom', 'dailyWord'];
 
   return keywords.map((keyword) => ({
     keyword, // 각 keyword 값을 반환
@@ -14,7 +15,7 @@ export default function relayspeakingKeywordPage() {
     <div>
       {/* 헤더 */}
       <header className="h-[6.25rem] relative py-[1.625rem] border-b border-[#9879451a]">
-        <Link href={'/'} className="absolute ">
+        <Link href={'/relayspeaking'} className="absolute ">
           <img src="/images/뒤로가기.svg" alt="logo" className="pl-5 sm:pl-[7.5rem]" />
         </Link>
         <div className="flex-center gap-[477px] h-12">
@@ -23,7 +24,9 @@ export default function relayspeakingKeywordPage() {
           </Link>
         </div>
       </header>
-      <p>아직 준비중이에요 금방 완성시킬게요!</p>
+
+      {/* 본문 */}
+      <RelayspeakingComponent />
     </div>
   );
 }
