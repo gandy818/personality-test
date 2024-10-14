a450f946b6b02b19c5eab2f70676b92581e456da
gandy818 <gandy818@gmail.com>
2024-10-14 17:53:56 +0900

    feat: 파비콘, 카톡 썸네일 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,29 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: '모두다블럭',
-  description: '술자리게임,단체게임,게임,예능게임,게임사이트,예능게임사이트',
+  description: '쉽고 간편하게 함께 즐기는 단체 게임으로 신나는 시간을 만들어보자!',
+  viewport: 'width=device-width, initial-scale=1',
+  openGraph: {
+    url: 'https://www.zeround.com',
+    title: '모두다블럭',
+    description: '쉽고 간편하게 함께 즐기는 단체 게임으로 신나는 시간을 만들어보자!',
+    images: [
+      {
+        url: '/images/카카오톡공유썸네일.png',
+        width: 400,
+        height: 210,
+        alt: '모두다블럭',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+  },
 };
-
-export const viewport = 'width=device-width, initial-scale=1';
 
 export default function RootLayout({
   children,
@@ -15,13 +32,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-[#FFF8EC] custom-cursor">
-      <head>
+      <Head>
+        {/* Google AdSense 스크립트 */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2998824044846392"
           crossOrigin="anonymous"
         ></script>
-      </head>
+      </Head>
       <body>{children}</body>
     </html>
   );
